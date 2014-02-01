1a57566128d22d0fbdfa8608f476d4e0d17b75b7
Tommi Reiman <tommi.reiman@metosin.fi>
2014-02-01 11:14:53 +0200

    move common code to compojure-api

--- src/ring/swagger/common.clj
+++ src/ring/swagger/common.clj
@@ -1,23 +1,6 @@
 (ns ring.swagger.common
   (:use [clojure.walk :as walk]))
 
-(defn path-vals
-  "Returns vector of tuples containing path vector to the value and the value."
-  [m]
-  (letfn
-    [(pvals [l p m]
-       (reduce
-         (fn [l [k v]]
-           (if (map? v)
-             (pvals l (conj p k) v)
-             (cons [(conj p k) v] l)))
-         l m))]
-    (pvals [] [] m)))
-
-(defn assoc-in-path-vals
-  "Re-created a map from it's path-vals extracted with (path-vals)."
-  [c] (reduce (partial apply assoc-in) {} c))
-
 (defmacro fn->
   "Creates a function that threads on input with some->"
   [& body] `(fn [x#] (some-> x# ~@body)))
@@ -25,10 +8,6 @@
 (defmacro fn->>
   "Creates a function that threads on input with some->>"
   [& body] `(fn [x#] (some->> x# ~@body)))
-
-(defn ->map
-  "Converts a map-like form (list of tuples, record a map) into a map."
-  [m] (into {} m))
 
 (defn remove-empty-keys
   "removes empty keys from a map"
@@ -50,21 +29,6 @@
     (var? x) (var-get x)
     (symbol? x) (eval x)
     :else x))
-
-(defmacro re-resolve
-  "Extracts original var from a (potemkined) var or a symbol or returns nil"
-  [x]
-  (let [evaluated (if (symbol? x) x (eval x))
-        resolved  (cond
-                    (var? evaluated)    evaluated
-                    (symbol? evaluated) (resolve evaluated)
-                    :else       nil)
-        metadata  (meta resolved)]
-    (if metadata
-      (let [s (symbol (str (:ns metadata) "/" (:name metadata)))]
-        `(var ~s)))))
-
-(defn eval-re-resolve [x] (eval `(re-resolve ~x)))
 
 (defn extract-parameters
   "Extract parameters from head of the list. Parameters can be:
