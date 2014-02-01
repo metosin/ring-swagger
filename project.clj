4a0aa45372f126e875af5193588dedf7cedaf974
Tommi Reiman <tommi.reiman@metosin.fi>
2014-02-01 11:15:35 +0200

    project.clj: compojure-api -> ring-swagger

--- project.clj
+++ project.clj
@@ -1,22 +1,17 @@
-(defproject metosin/compojure-api "0.0.1"
-  :description "Compojure Api"
-  :url "https://github.com/metosin/compojure-api"
+(defproject metosin/ring-swagger "0.0.1"
+  :description "Swagger Spec for Ring Apps"
+  :url "https://github.com/metosin/ring-swagger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [potemkin "0.3.4"]
                  [cheshire "5.3.1"]
                  [camel-snake-kebab "0.1.2"]
                  [prismatic/schema "0.2.0"]
-                 [metosin/ring.swagger-ui "0.0.1"]
-                 [compojure "1.1.6"]]
+                 [ring/ring-core "1.2.1"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler compojure.api.example.handler/app}
   :profiles {:dev {:plugins [[lein-clojars "0.9.1"]
                              [lein-midje "3.1.1"]]
-                   :dependencies [[ring-mock "0.1.5"]
-                                  [javax.servlet/servlet-api "2.5"]
-                                  [midje "1.6.0"]
-                                  [clj-http "0.7.8" :exclusions [commons-codec]]]}})
+                   :dependencies [[midje "1.6.0"]]}})
