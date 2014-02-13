9090f36bb4fd791e554652062741ba0f545db8ce
Tommi Reiman <tommi.reiman@metosin.fi>
2014-02-13 08:36:01 +0200

    bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger "0.2.0"
+(defproject metosin/ring-swagger "0.3.0"
   :description "Swagger Spec for Ring Apps"
   :url "https://github.com/metosin/ring-swagger"
   :license {:name "Eclipse Public License"
