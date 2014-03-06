4c7866e29d34068c7dc1bc301cda1cb619b42e69
Tommi Reiman <tommi@iki.fi>
2014-03-06 16:40:16 +0200

    coerce for dates

--- test/ring/swagger/coerce_test.clj
+++ test/ring/swagger/coerce_test.clj
@@ -1,7 +1,23 @@
 (ns ring.swagger.coerce-test
   (:require [midje.sweet :refer :all]
             [schema.core :as s]
-            [ring.swagger.coerce :refer :all]))
+            [ring.swagger.coerce :refer :all])
+  (:import [org.joda.time LocalDate DateTime]
+    [java.util Date]))
+
+(fact "json coercions"
+  (let [c (coercer :json)]
+
+    (fact "Date with and without millis"
+      ((c Date) "2014-02-18T18:25:37.456Z") => (partial instance? Date)
+      ((c Date) "2014-02-18T18:25:37Z") => (partial instance? Date))
+
+    (fact "DateTime with and without millis"
+      ((c DateTime) "2014-02-18T18:25:37.456Z") => (partial instance? DateTime)
+      ((c DateTime) "2014-02-18T18:25:37Z") => (partial instance? DateTime))
+
+    (fact "LocalDate"
+      ((c LocalDate) "2014-02-19") => (partial instance? LocalDate))))
 
 (fact "query coercions"
   (let [c (coercer :query)]
