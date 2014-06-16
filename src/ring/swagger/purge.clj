e389f1b6ec981c862e57e83da982be2cf80383a5
Tommi Reiman <tommi@iki.fi>
2014-06-16 21:26:50 +0300

    rewrite to use non-tail recursion

--- src/ring/swagger/purge.clj
+++ src/ring/swagger/purge.clj
@@ -3,59 +3,59 @@
             [clojure.string :as st]
             [camel-snake-kebab :refer [->CamelCase]]))
 
-(defn- valid-container? [x]
+(defn contain [x y]
+  (cond
+    (set? x) (-> x (disj (first x)) (conj y))
+    (sequential? x) (-> x pop (conj y))))
+
+(defn valid-container? [x]
   (and
     (or (sequential? x)
         (set? x))
     (= (count x) 1)))
 
-(defn- plain-map? [x]
-  (and (instance? clojure.lang.APersistentMap x) ;; need to filter out Schema records
-       (not (s/schema-name x)))) ;; and named schemas
+(defn named-schema? [x]
+  (boolean (s/schema-name x)))
 
-(defn- root? [keys] (not (butlast keys)))
+(defn plain-map? [x]
+  (instance? clojure.lang.APersistentMap x))
+
+(defn root? [keys] (not (butlast keys)))
 
 (defn- full-name [path]
   (->> path
-       (remove number?)
        (map name)
        (map ->CamelCase)
        (apply str)
        symbol))
 
+(defn collect-schemas [keys schema]
+  (cond
+
+    (map? schema)
+    (if (and (not (root? keys)) (named-schema? schema))
+      schema
+      (with-meta
+        (merge {}
+               (into {}
+                     (for [[k v] schema
+                           :let [keys (conj keys k)]]
+                       [k (collect-schemas keys v)])))
+        {:name (full-name keys)}))
+
+    (valid-container? schema)
+    (contain schema (collect-schemas keys (first schema)))
+
+    :else schema))
+
 (defn with-sub-schemas [schema]
-  (let [schemas (atom {})]
-    (letfn [
-            (collect-schemas [schema keys]
-              (cond
-
-                ;; root or anonoymous map
-                (or (root? keys) (plain-map? schema))
-                (do
-                  (swap! schemas assoc keys schema)
-                  (doseq [[k v] schema]
-                    (collect-schemas v (conj keys k))))
-
-                ;; valid container
-                (valid-container? schema)
-                (collect-schemas (first schema) (conj keys 0))))]
-      (collect-schemas schema [(s/schema-name schema)])
-      (reduce (fn [schema [[_ & keys :as all-keys] _]]
-                (if-not (seq keys)
-                  schema
-                  (update-in schema keys
-                             (fn [subschema]
-                               (if-not subschema
-                                 subschema
-                                 (with-meta subschema {:name (full-name all-keys)}))))))
-              schema
-              @schemas))))
+  (collect-schemas [(s/schema-name schema)] schema))
 
 ;;
 ;; Spiking
 ;;
 
-(comment
+
 (s/defschema OrderType {:name String})
 
 (s/defschema Order {:id String
@@ -63,15 +63,16 @@
                     :type OrderType
                     :mass [{:unit (s/enum :kg :g)
                             :value Long}]
+                    :mass2 #{{:a Long}}
                     :address {:street String
                               :zip Long
-                              :country (s/enum :fi :po)
+                              ;; :country (s/enum :fi :po)
                               :type {:a :b}}})
 
 (def Order2 (with-sub-schemas Order))
 
 (-> Order2 (s/schema-name))
 (-> Order2 :mass first (s/schema-name))
+(-> Order2 :mass2 first (s/schema-name))
 (-> Order2 :address :type (s/schema-name))
 (clojure.pprint/pprint Order2)
-)
