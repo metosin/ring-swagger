6844f0de3fd762a5c46822a01c61a8a473972167
Tommi Reiman <tommi@iki.fi>
2014-06-16 23:02:03 +0300

    impl to have common schema helpers

---
+++ src/ring/swagger/impl.clj
@@ -0,0 +1,42 @@
+(ns ring.swagger.impl
+  (:require [schema.core :as s]))
+
+;;
+;; Schema containers
+;;
+
+(defn asserted-container [x]
+  (assert (= (count x) 1) "schema containers can only one element.") x)
+
+(defn contain [x y]
+  (cond
+    (set? x) (-> x (disj (first x)) (conj y))
+    (sequential? x) (-> x pop (conj y))))
+
+(defn valid-container? [x]
+  (and (or (sequential? x) (set? x))
+       (asserted-container x)))
+
+(defn update-schema [x f]
+  (if (valid-container? x)
+    (contain x (f (first x)))
+    (f x)))
+
+;;
+;; Other
+;;
+
+(defn required-keys [schema]
+  (filterv s/required-key? (keys schema)))
+
+(defn strict-schema
+  "removes open keys from schema"
+  [schema]
+  {:pre [(map? schema)]}
+  (dissoc schema s/Keyword))
+
+(defn loose-schema
+  "add open keys for top level schema"
+  [schema]
+  {:pre [(map? schema)]}
+  (assoc schema s/Keyword s/Any))
---
+++ test/ring/swagger/impl_test.clj
@@ -0,0 +1,11 @@
+(ns ring.swagger.impl-test
+  (:require [midje.sweet :refer :all]
+            [ring.swagger.impl :refer :all]
+            [schema.core :as s]))
+
+(fact "scrict-schema strips open keys"
+      (strict-schema {s/Keyword s/Any :s String}) => {:s String})
+
+(fact "loose-schema adds open keys to top-level"
+      (loose-schema {:s String}) => {s/Keyword s/Any :s String})
+
