f246699008939fd05b9b890d164096de45df932a
Tommi Reiman <tommi@iki.fi>
2014-06-16 23:23:02 +0300

    clean up purge

--- src/ring/swagger/purge.clj
+++ src/ring/swagger/purge.clj
@@ -1,52 +1,41 @@
 (ns ring.swagger.purge
   (:require [schema.core :as s]
             [clojure.string :as st]
+            [ring.swagger.impl :refer :all]
+            [ring.swagger.schema :refer :all]
             [camel-snake-kebab :refer [->CamelCase]]))
 
-(defn contain [x y]
-  (cond
-    (set? x) (-> x (disj (first x)) (conj y))
-    (sequential? x) (-> x pop (conj y))))
-
-(defn valid-container? [x]
-  (and
-    (or (sequential? x)
-        (set? x))
-    (= (count x) 1)))
-
-(defn named-schema? [x]
-  (boolean (s/schema-name x)))
-
-(defn plain-map? [x]
-  (instance? clojure.lang.APersistentMap x))
-
-(defn root? [keys] (not (butlast keys)))
-
-(defn- full-name [path]
-  (->> path
-       (map name)
-       (map ->CamelCase)
-       (apply str)
-       symbol))
-
-(defn collect-schemas [keys schema]
-  (cond
-
-    (map? schema)
-    (if (and (not (root? keys)) (named-schema? schema))
-      schema
-      (with-meta
-        (merge {}
-               (into {}
-                     (for [[k v] schema
-                           :let [keys (conj keys k)]]
-                       [k (collect-schemas keys v)])))
-        {:name (full-name keys)}))
-
-    (valid-container? schema)
-    (contain schema (collect-schemas keys (first schema)))
-
-    :else schema))
-
 (defn with-sub-schemas [schema]
-  (collect-schemas [(s/schema-name schema)] schema))
+  (letfn [(full-name [path]
+            (->> path (map name) (map ->CamelCase) (apply str) symbol))
+          (collect-schemas [keys schema]
+            (cond
+              (map? schema)
+              (if (and (seq (pop keys)) (named-schema? schema))
+                schema
+                (with-meta
+                  (merge {}
+                         (into {}
+                               (for [[k v] schema
+                                     :let [keys (conj keys k)]]
+                                 [k (collect-schemas keys v)])))
+                  {:name (full-name keys)}))
+
+              (valid-container? schema)
+              (contain schema (collect-schemas keys (first schema)))
+
+              :else schema))]
+    (collect-schemas [(or (s/schema-name schema)
+                          (gensym "schema"))] schema)))
+
+
+(def Order {:customer {:name String
+                       :address {:street String
+                                 :city {:name String}}}
+            :lines [{:id String
+                     :tags #{{:name String}}}]})
+
+(def Order2 (with-sub-schemas Order))
+
+(-> Order2 :customer :address :city s/schema-name)
+(-> Order2 :lines first :tags first s/schema-name)
