a7da6486c5002994fb63c1bcab9ab86195ef68c9
Tommi Reiman <tommi@iki.fi>
2014-10-05 20:19:04 +0300

    separate swagger-spec schemas into separate files

--- src/ring/swagger/core.clj
+++ src/ring/swagger/core.clj
@@ -11,6 +11,7 @@
             [ring.swagger.coerce :as coerce]
             [ring.swagger.common :refer :all]
             [ring.swagger.json-schema :as jsons]
+            [ring.swagger.spec :as spec]
             [cheshire.generate :refer [add-encoder]]
             [camel-snake-kebab.core :refer [->camelCase]])
   (:import [com.fasterxml.jackson.core JsonGenerator]))
@@ -255,123 +256,8 @@
 ;; 2.0
 ;;
 
-;;
-;; helpers
-;;
-
-(defn regexp [r n] (s/pred (partial re-find r) n))
-(defn valid-response-key? [x] (or (= :default x) (integer? x)))
 (def Anything {s/Keyword s/Any})
 (def Nothing {})
-
-;;
-;; schemas
-;;
-
-
-(def VendorExtension {(s/pred (fn->> name (re-find #"^x-")) "vendor extension") s/Any})
-
-(s/defschema ExternalDocs {:url s/Str
-                           (s/optional-key :description) s/Str})
-
-(s/defschema Info (merge
-                    VendorExtension
-                    {:version s/Str
-                     :title   s/Str
-                     (s/optional-key :description) s/Str
-                     (s/optional-key :termsOfService) s/Str
-                     (s/optional-key :contact) {(s/optional-key :name) s/Str
-                                                (s/optional-key :url) s/Str
-                                                (s/optional-key :email) s/Str}
-                     (s/optional-key :licence) {:name s/Str
-                                                (s/optional-key :url) s/Str}}))
-
-(s/defschema Tag (s/either s/Str s/Keyword))
-(s/defschema Schema s/Any)
-(s/defschema Scheme (s/enum :http :https :ws :wss))
-(s/defschema SerializableType {(s/optional-key :type) (s/enum :string :number :boolean :integer :array :file)
-                               (s/optional-key :format) s/Str
-                               (s/optional-key :items) s/Any
-                               (s/optional-key :collectionFormat) s/Str})
-
-(s/defschema Example s/Any)   ; TODO
-(s/defschema Schema s/Any)    ; TODO
-
-(s/defschema Parameter
-             (s/either
-               (merge
-                 VendorExtension
-                 {:name s/Str
-                  :in (s/enum :query, :header, :path, :formData)
-                  (s/optional-key :description) s/Str
-                  (s/optional-key :required) s/Bool
-                  (s/optional-key :type) (s/enum :string, :number, :boolean, :integer, :array)
-                  (s/optional-key :format) s/Str
-                  (s/optional-key :items) s/Any ; TODO https://github.com/reverb/swagger-spec/blob/master/schemas/v2.0/schema.json#L401
-                  (s/optional-key :collectionFormat) s/Str})
-               (merge
-                 VendorExtension
-                 {:name s/Str
-                  :in (s/enum :body)
-                  (s/optional-key :description) s/Str
-                  (s/optional-key :required) s/Bool
-                  (s/optional-key :schema) String}))) ; TODO should be a valid model ref
-
-(s/defschema Response {:description s/Str
-                       (s/optional-key :schema) Schema
-                       (s/optional-key :headers) [SerializableType]
-                       (s/optional-key :examples) Example})
-
-(s/defschema Responses (merge
-                         ;VendorExtension TODO: More than one non-optional/required key schemata
-                         {(s/pred valid-response-key?) Response}))
-
-
-(s/defschema Ref {:$ref s/Str})
-
-(s/defschema Operation {(s/optional-key :tags) [Tag]
-                        (s/optional-key :summary) s/Str
-                        (s/optional-key :description) s/Str
-                        (s/optional-key :externalDocs) ExternalDocs
-                        (s/optional-key :operationId) s/Str
-                        (s/optional-key :consumes) [s/Str]
-                        (s/optional-key :produces) [s/Str]
-                        (s/optional-key :parameters) [Parameter] ;TODO: (s/either Parameter Ref) -> https://github.com/reverb/swagger-spec/blob/master/schemas/v2.0/schema.json#L236
-                        :responses Responses
-                        (s/optional-key :schemes) [Scheme]
-                                                                 ;(s/optional-key :security) s/Any
-                        })
-(s/defschema PathItem {(s/optional-key :ref) s/Str
-                       (s/optional-key :get) Operation
-                       (s/optional-key :put) Operation
-                       (s/optional-key :post) Operation
-                       (s/optional-key :delete) Operation
-                       (s/optional-key :options) Operation
-                       (s/optional-key :head) Operation
-                       (s/optional-key :patch) Operation
-                       (s/optional-key :parameters) [Parameter]})
-(s/defschema Paths {(regexp #"^/.*[^\/]$" "valid path") PathItem})
-(s/defschema Definitions {s/Keyword {s/Keyword s/Any}}) ; TODO validate for real?
-
-#_(s/defschema Parameters s/Any)
-#_(s/defschema Security s/Any)
-
-
-(s/defschema SwaggerDocs {:swagger (s/enum 2.0)
-                          :info Info
-                          ;(s/optional-key :externalDocs) ExternalDocs
-                          (s/optional-key :host) s/Str
-                          (s/optional-key :basePath) s/Str
-                          (s/optional-key :schemes) [Scheme]
-                          (s/optional-key :consumes) [s/Str]
-                          (s/optional-key :produces) [s/Str]
-                          :paths Paths
-                          (s/optional-key :definitions) Definitions
-                          ;(s/optional-key :parameters) Parameters
-                          ;(s/optional-key :responses) Responses
-                          ;(s/optional-key :security) Security
-                          (s/optional-key :tags) [Tag]
-                          })
 
 ;;
 ;; defaults
@@ -422,7 +308,7 @@
                                                          :schema {:code Long}}}
                                    :schemes [:http]}]}})
 
-(s/validate SwaggerDocs
+(s/validate spec/SwaggerDocs
             (:body (swagger-docs
                      {:info {:version "version"
                              :title "title"
