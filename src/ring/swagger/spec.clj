6bd4bb5ec68aeabd28636a31c1af7dd9d7ce8d0b
Miika Pihlaja <miika@metosin.fi>
2014-12-10 01:01:54 +0200

    parameters wip

--- src/ring/swagger/spec.clj
+++ src/ring/swagger/spec.clj
@@ -56,7 +56,8 @@
                   :in (s/enum :query, :header, :path, :formData)
                   (s/optional-key :description) s/Str
                   (s/optional-key :required) s/Bool
-                  (s/optional-key :type) (s/enum :string, :number, :boolean, :integer, :array)
+                  (s/optional-key :type) #_(s/enum :string, :number, :boolean, :integer, :array)
+                  (s/enum "string" "number" "boolean" "integer" "array" "file")
                   (s/optional-key :format) s/Str
                   (s/optional-key :items) s/Any ; TODO: https://github.com/reverb/swagger-spec/blob/master/schemas/v2.0/schema.json#L401
                   (s/optional-key :collectionFormat) s/Str})
