67f77cf2a66a492a6c4cb6b2889840f8f525e316
Juho Teperi <juho.teperi@iki.fi>
2014-12-27 15:46:14 +0200

    Use new Travis infrastructure

--- project.clj
+++ project.clj
@@ -23,6 +23,6 @@
                                   [ring-mock "0.1.5"]
                                   [metosin/ring-swagger-ui "2.0.17"]
                                   [javax.servlet/servlet-api "2.5"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.7"]
             "test-ancient" ["midje"]})
