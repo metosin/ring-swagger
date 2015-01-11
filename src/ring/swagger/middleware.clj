b85f11e73d98241a3ba1fd0858df21730b879ad5
Tommi Reiman <tommi@iki.fi>
2015-01-11 21:43:49 +0200

    cleanup, catch-validation-errors takes option :error-handler for custom error reporting

--- src/ring/swagger/middleware.clj
+++ src/ring/swagger/middleware.clj
@@ -1,27 +1,34 @@
 (ns ring.swagger.middleware
   (:require [slingshot.slingshot :refer [try+]]
             [schema.utils :as su]
+            [plumbing.core :refer [for-map]]
             [clojure.walk :refer :all]
-            [ring.util.http-response :refer [bad-request]]))
+            [ring.util.http-response :refer [bad-request]])
+  (:import (schema.utils ValidationError)))
 
 (defn stringify-error [error]
   (postwalk
     (fn [x]
       (if-not (map? x)
         x
-        (into {}
-              (for [[k v] x]
-                [k (cond
-                     (instance? schema.utils.ValidationError v) (str (su/validation-error-explain v))
-                     (symbol? v) (str v)
-                     :else v)]))))
+        (for-map [[k v] x]
+          k (cond
+              (instance? ValidationError v) (str (su/validation-error-explain v))
+              (symbol? v) (str v)
+              :else v))))
     error))
 
+(defn default-error-handler [error]
+  (bad-request {:errors (stringify-error error)}))
+
 (defn catch-validation-errors
-  [handler]
+  "Catches thrown ring-swagger validation errors and turns them into valid
+   error respones. Accepts the following options:
+
+   :error-handler - a function of schema.utils.ErrorContainer -> response"
+  [handler & {:keys [error-handler] :or {error-handler default-error-handler}}]
   (fn [request]
     (try+
       (handler request)
       (catch [:type :ring.swagger.schema/validation] {:keys [error]}
-        (bad-request
-          {:errors (stringify-error error)})))))
+        (error-handler error)))))
