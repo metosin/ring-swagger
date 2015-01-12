bde134befa65838e536b4573881138a97899451e
Tommi Reiman <tommi@iki.fi>
2015-01-12 08:31:25 +0200

    better docs for coerce & coerce!

--- src/ring/swagger/schema.clj
+++ src/ring/swagger/schema.clj
@@ -33,19 +33,26 @@
   [x] (boolean (s/schema-name x)))
 
 (defn coerce
-  "Coerces a value against a schema using enhanced json-coercion.
-   If no errors, returns the value, otherwise returns ValidationError."
-  ([model value] (coerce model value :json))
-  ([model value type]
-    ((sc/coercer (value-of model) (coerce/coercer type)) value)))
+  "Coerces a value against a schema using a given coerser. If no errors,
+   returns the coerced value, otherwise returns ValidationError.
+
+   Optional third parameter is a dispatch value to ring.swagger.coerce/coercer,
+   defaults to :json"
+  ([schema value] (coerce schema value :json))
+  ([schema value type]
+    (let [coercer (if (keyword? type) (coerce/coercer type) type)]
+      ((sc/coercer (value-of schema) coercer) value))))
 
 (defn coerce!
-  "Coerces a value against a schema using enhanced json-coercion.
-   If no errors, returns the value, otherwise slingshots a
-   schema.utils.ErrorContainer enriched with :type ::validation"
-  ([model value] (coerce! model value :json))
-  ([model value type]
-    (let [result (coerce model value type)]
+  "Coerces a value against a schema using a given coerser. If no errors,
+   returns the coerced value, otherwise throws a schema.utils.ErrorContainer
+   enriched with :type ::validation.
+
+   Optional third parameter is a dispatch value to ring.swagger.coerce/coercer,
+   defaults to :json"
+  ([schema value] (coerce! schema value :json))
+  ([schema value type]
+    (let [result (coerce schema value type)]
       (if (error? result)
         (throw+ (assoc result :type ::validation))
         result))))
