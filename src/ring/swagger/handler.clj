da4918cc9b9fbc26eb263f76f78ef749c6ea70ae
Tommi Reiman <tommi@iki.fi>
2015-04-07 18:35:53 +0300

    handlers, part2

--- src/ring/swagger/handler.clj
+++ src/ring/swagger/handler.clj
@@ -1,4 +1,30 @@
-(ns ring.swagger.handler)
+(ns ring.swagger.handler
+  (:require [plumbing.fnk.pfnk :as pfnk]
+            [schema.core :as s]
+            [plumbing.core :refer [fnk for-map]]))
 
-(defn handler [meta f]
-  (with-meta f meta))
+(def ring2swagger
+  {:query-params :query
+   :path-params :path
+   :body-params :body
+   :header-params :header})
+
+(defn swagger-parameters [schema]
+  {:parameters (for-map [[ring-key swagger-key] ring2swagger
+                         :let [ring-value (get-in schema [:request ring-key])]
+                         :when ring-value]
+                 swagger-key ring-value)})
+
+;;
+;; Public API
+;;
+
+(defn handler
+  ([f]
+   (handler {} f))
+  ([handler-meta f]
+   (let [f-meta (meta f)
+         schema (if (:schema f-meta) (pfnk/input-schema f))
+         imeta (some-> schema swagger-parameters)
+         full-meta (merge imeta handler-meta)]
+     (with-meta f full-meta))))
--- test/ring/swagger/handler_test.clj
+++ test/ring/swagger/handler_test.clj
@@ -1,21 +1,55 @@
 (ns ring.swagger.handler-test
   (:require [midje.sweet :refer :all]
             [ring.swagger.handler :refer :all]
+            [plumbing.core :refer [fnk]]
+            [ring.util.http-response :refer [ok]]
             [schema.core :as s]))
 
-(fact "handler with meta"
-  (let [metadata {:tags         [:programmer]
-                  :summary      "programming endpoint"
-                  :description  "no gardening here"
+(fact "fn-handler"
+  (let [endpoint (handler identity)]
+
+    (endpoint ..request..) => ..request..
+    (meta endpoint) => {}))
+
+(fact "fn-handler with metadata"
+  (let [metadata {:tags [:programmer]
+                  :summary "programming endpoint"
+                  :description "no gardening here"
                   :externalDocs {:url "http://www.metosin.fi"
                                  :description "link to it"}
-                  :parameters   {:query    {:language s/Str, :level s/Int}
-                                 :path     {:id s/Str}}
-                  :responses    {200      {:description "ok"}
-                                 :default {:description "error"
-                                           :schema      {:code Long}}}}
+                  :parameters {:query {:language s/Str, :level s/Int}
+                               :path {:id s/Str}}
+                  :responses {200 {:description "ok"}
+                              :default {:description "error"
+                                        :schema {:code Long}}}}
         endpoint (handler metadata identity)]
+
     (endpoint ..request..) => ..request..
     (meta endpoint) => metadata))
 
+(fact "fnk handler"
+  (let [endpoint (handler
+                   (fnk
+                     [[:request
+                       [:query-params x :- s/Int, y :- s/Int]]]
+                     (ok [x y])))]
 
+    (endpoint {:request {:query-params {:x 1 :y 2}}}) => (contains {:body [1 2]})
+    (meta endpoint) => {:parameters {:query {:x s/Int, :y s/Int, s/Keyword s/Any}}}))
+
+(fact "fnk-handler with metadata"
+  (let [endpoint (handler
+                   {:tags [:programmer]
+                    :responses {200 {:schema [s/Int]}}}
+                   (fnk
+                     [[:request
+                       [:query-params x :- s/Int, y :- s/Int]
+                       [:path-params z :- s/Int]]]
+                     (ok [x y z])))]
+
+    (endpoint {:request {:query-params {:x 1 :y 2}
+                         :path-params {:z 3}}}) => (contains {:body [1 2 3]})
+    (meta endpoint) => {:tags [:programmer]
+                        :parameters {:query {:x s/Int, :y s/Int, s/Keyword s/Any}
+                                     :path {:z s/Int, s/Keyword s/Any}}
+                        :responses {200 {:schema [s/Int]}}}))
