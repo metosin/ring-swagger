4f1340852916ea8bce918e567cd3b9bc8ecb537b
Tommi Reiman <tommi@iki.fi>
2015-04-07 22:20:56 +0300

    load schema-file on startup

--- test/ring/swagger/validator.clj
+++ test/ring/swagger/validator.clj
@@ -3,7 +3,5 @@
             [scjsv.core :as v]))
 
 ;; "http://swagger.io/v2/schema.json"
-(def schema-string
-  (delay (slurp (io/file (io/resource "ring/swagger/v2.0_schema.json")))))
-
-(defn validate [data] (v/validate @schema-string data))
+(def validate
+  (partial v/validate (slurp (io/resource "ring/swagger/v2.0_schema.json"))))
