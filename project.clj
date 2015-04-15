0f2acf0b5181de5e3e69692bd9c2bc8c14e01783
Tommi Reiman <tommi@iki.fi>
2015-04-15 09:52:37 +0300

    snapshot it

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger "0.19.5"
+(defproject metosin/ring-swagger "0.19.6-SNAPSHOT"
   :description "Swagger Spec for Ring Apps"
   :url "https://github.com/metosin/ring-swagger"
   :license {:name "Eclipse Public License"
