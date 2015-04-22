18453f08d06447d7c40967f5990aa24b0df27d45
Juho Teperi <juho.teperi@iki.fi>
2015-04-22 22:29:16 +0300

    File type

--- src/ring/swagger/json_schema.clj
+++ src/ring/swagger/json_schema.clj
@@ -90,6 +90,24 @@
     {:type "string" :pattern (str e)}
     {:type "string" :format "regex"}))
 
+(defrecord File [m]
+  schema.core.Schema
+  (walker [this]
+    (let [sub-walker (schema.core/subschema-walker m)]
+      (clojure.core/fn [x]
+       (if (schema.utils/error? x)
+         x
+         (sub-walker x)))))
+  (explain [this] (cons 'file m)))
+
+(def file
+  (File. {:filename s/Str
+          :size s/Int
+          (s/optional-key :tempfile) java.io.File
+          :content-type s/Str}))
+
+(defmethod json-type File            [_] {:type "file"})
+
 ;; Schemas
 ;; Convert the most common predicates by mapping fn to Class
 (def predicate-to-class {integer? java.lang.Long
