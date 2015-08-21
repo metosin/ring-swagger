520cd71ef80e60d2003009acdc0ee56fa1e67c45
Tommi Reiman <tommi@iki.fi>
2015-08-21 09:10:46 +0300

    FIXME for having json-encodings hiddenly imported to ring.swagger.swagger2.

--- src/ring/swagger/swagger2.clj
+++ src/ring/swagger/swagger2.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string :as str]
             [schema.core :as s]
             [plumbing.core :refer [for-map]]
+            ; the json-encodings, FIXME: global side effects -> separate import
             ring.swagger.json
             [ring.swagger.common :refer :all]
             [ring.swagger.json-schema :as jsons]
