fd3788c2b25c9ac69d60364cea2ab2f09e3778db
Juho Teperi <juho.teperi@iki.fi>
2015-11-20 22:41:21 +0200

    Bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger "0.22.0"
+(defproject metosin/ring-swagger "0.22.1-SNAPSHOT"
   :description "Swagger Spec for Ring Apps"
   :url "https://github.com/metosin/ring-swagger"
   :license {:name "Eclipse Public License"
