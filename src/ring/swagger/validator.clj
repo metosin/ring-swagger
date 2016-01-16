1a71597cee8a1aaecd620421d71ca9b4b05510bd
Tommi Reiman <tommi@iki.fi>
2016-01-16 14:59:09 +0200

    Use standalone-swagger-schema in validation

--- src/ring/swagger/validator.clj
+++ src/ring/swagger/validator.clj
@@ -5,6 +5,5 @@
 ; https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/schemas/v2.0/schema.json
 ; http://json-schema.org/draft-04/schema
 
-; TODO: how to reference to a local copy of the ring/swagger/json-schema.json?
 (def validate
-  (v/validator (slurp (io/resource "ring/swagger/swagger-schema.json"))))
+  (v/validator (slurp (io/resource "ring/swagger/standalone-swagger-schema.json"))))
