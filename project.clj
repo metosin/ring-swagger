b41b354dc2e48774735eda46c9bec986d1422ccc
Tommi Reiman <tommi@iki.fi>
2016-02-11 23:17:42 +0200

    Use Clojure 1.8.0, test also 1.7.0

--- project.clj
+++ project.clj
@@ -20,17 +20,17 @@
                              [lein-ring "0.9.7"]
                              [lein-midje "3.2"]
                              [funcool/codeina "0.3.0"]]
-                   :dependencies [[org.clojure/clojure "1.7.0"]
+                   :dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.8.3"]
                                   [ring-mock "0.1.5"]
                                   [metosin/ring-swagger-ui "2.1.4-0"]
                                   [javax.servlet/servlet-api "2.5"]
                                   ; Required when using with Java 1.6
                                   [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC5"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :codeina {:sources ["src"]
             :target "gh-pages/doc"
             :src-dir-uri "http://github.com/metosin/ring-swagger/blob/master/"
             :src-linenum-anchor-prefix "L" }
-  :aliases {"all" ["with-profile" "dev:dev,1.8"]
+  :aliases {"all" ["with-profile" "dev:dev,1.7"]
             "test-ancient" ["midje"]})
