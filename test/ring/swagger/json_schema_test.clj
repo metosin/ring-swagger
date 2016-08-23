869f687be6bcfc3f63bfce829ea557323a8dfe5c
Francesco Bellomi <francesco.bellomi@gmail.com>
2016-08-23 08:59:28 +0200

    indentation + swagger2 test

--- test/ring/swagger/json_schema_test.clj
+++ test/ring/swagger/json_schema_test.clj
@@ -18,20 +18,20 @@
 
 (facts "type transformations"
   (facts "java types"
-         (rsjs/->swagger Integer) => {:type "integer" :format "int32"}
-         (rsjs/->swagger Long) => {:type "integer" :format "int64"}
-         (rsjs/->swagger Double) => {:type "number" :format "double"}
-         (rsjs/->swagger Number) => {:type "number" :format "double"}
-         (rsjs/->swagger Symbol) => {:type "string"}
-         (rsjs/->swagger String) => {:type "string"}
-         (rsjs/->swagger Boolean) => {:type "boolean"}
-         (rsjs/->swagger Date) => {:type "string" :format "date-time"}
-         (rsjs/->swagger DateTime) => {:type "string" :format "date-time"}
-         (rsjs/->swagger LocalDate) => {:type "string" :format "date"}
-         (rsjs/->swagger LocalTime) => {:type "string" :format "time"}
-         (rsjs/->swagger Pattern) => {:type "string" :format "regex"}
-         (rsjs/->swagger #"[6-9]") => {:type "string" :pattern "[6-9]"}
-         (rsjs/->swagger UUID) => {:type "string" :format "uuid"})
+    (rsjs/->swagger Integer) => {:type "integer" :format "int32"}
+    (rsjs/->swagger Long) => {:type "integer" :format "int64"}
+    (rsjs/->swagger Double) => {:type "number" :format "double"}
+    (rsjs/->swagger Number) => {:type "number" :format "double"}
+    (rsjs/->swagger Symbol) => {:type "string"}
+    (rsjs/->swagger String) => {:type "string"}
+    (rsjs/->swagger Boolean) => {:type "boolean"}
+    (rsjs/->swagger Date) => {:type "string" :format "date-time"}
+    (rsjs/->swagger DateTime) => {:type "string" :format "date-time"}
+    (rsjs/->swagger LocalDate) => {:type "string" :format "date"}
+    (rsjs/->swagger LocalTime) => {:type "string" :format "time"}
+    (rsjs/->swagger Pattern) => {:type "string" :format "regex"}
+    (rsjs/->swagger #"[6-9]") => {:type "string" :pattern "[6-9]"}
+    (rsjs/->swagger UUID) => {:type "string" :format "uuid"})
 
   (fact "schema types"
     (rsjs/->swagger s/Int) => {:type "integer" :format "int64"}
