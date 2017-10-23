426269678ff91c0a10eb7b863b49025044791a10
nha <nha@users.noreply.github.com>
2017-10-23 22:00:41 +0100

    fix: correct validation error - remove "void

--- src/ring/swagger/json_schema.clj
+++ src/ring/swagger/json_schema.clj
@@ -215,11 +215,11 @@
 
   schema.core.ConditionalSchema
   (convert [e _]
-    {:type "void" :x-oneOf (vec (keep (comp ->swagger second) (:preds-and-schemas e)))})
+    {:x-oneOf (vec (keep (comp ->swagger second) (:preds-and-schemas e)))})
 
   schema.core.CondPre
   (convert [e _]
-    {:type "void" :x-oneOf (mapv ->swagger (:schemas e))})
+    {:x-oneOf (mapv ->swagger (:schemas e))})
 
   schema.core.Constrained
   (convert [e _]
